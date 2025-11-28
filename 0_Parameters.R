3f7b6784ba543d8e00bc3a9de946651c8027059f
inesoriany <ines.khoun@ens.psl.eu>
2025-11-28 18:34:53 +0100

    diseases

--- 0_Parameters.R
+++ 0_Parameters.R
@@ -10,6 +10,7 @@ step_length <- 0.715*1e-3   # km
 ## WALKING SPEED (Barban et al, 2022) ----
 walk_speed <- 4.8  # km/h
 
+
 ## DRIVING SPEED (Kahlmeier, Götschi et al, 2017) ----
 paris_car_speed <- 31  # km/h 
 urban_car_speed <-  32
@@ -20,11 +21,52 @@ rural_car_speed <- 60
 #                          Diseases                          #
 ##############################################################
 
+# Disease name
+names_disease <- c(
+  "bc" = "Breast cancer incidence",
+  "cancer" = "Cancer incidence",
+  "cvd" = "Cardiovascular disease incidence",
+  "dem" = "Dementia",
+  "diab2" = "Type 2 diabetes",
+  "dep" = "Depressive symptoms",
+  "mort" = "All-cause mortality"
+)
+
+# Disease colour
+colors_disease <- c(
+  "bc" = "firebrick2",
+  "cancer" = "darkorange",      
+  "cvd" = "gold" ,
+  "dem" = "pink" ,
+  "diab2" = "palegreen3",
+  "dep" = "slateblue",
+  "mort" = "steelblue"
+)
+
+
+## BREAST CANCER ----
+# Relative risks (Monninkhof et al., 2007)
+ref_bc_w <- 60 
+rr_bc_women_lb <- .92 
+rr_bc_women <- .94
+rr_bc_women_ub <-.97
+
+ref_bc_m <- NA
+rr_bc_men_ub <-NA
+rr_bc_men <- NA
+rr_bc_men_lb <- NA
+
+
+
 ## DISABILITY WEIGHTS ----
 
-dem_dw_mid <-0.1518996
-dem_dw_low <-0.1250537
-dem_dw_up <-0.1758752
+bc_dw_mid <-0.06758792
+bc_dw_low <-0.05164746
+bc_dw_up <-0.083671
+
+cancer_dw_mid <- 0.05584116
+cancer_dw_low <- 0.08662941
+cancer_dw_up <- 0.03288360
 
 cvd_dw_mid <-0.0526328
 cvd_dw_low <-0.04023609
@@ -33,6 +75,10 @@ cvd_dw_up <-0.0645608
 diab2_dw_mid <- 0.06806817
 diab2_dw_low <-0.0504114
 diab2_dw_up <-0.08533913
+
+dem_dw_mid <-0.1518996
+dem_dw_low <-0.1250537
+dem_dw_up <-0.1758752
 
 mort_dw_mid <- 1
 mort_dw_low <- 1
