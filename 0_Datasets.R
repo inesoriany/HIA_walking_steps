443d594d12492e94b3a19f29784f7c0ff8291d35
inesoriany <ines.khoun@ens.psl.eu>
2025-11-28 15:29:53 +0100

    age group 10

--- 0_Datasets.R
+++ 0_Datasets.R
@@ -56,11 +56,48 @@ insee <- import(here("data", "INSEE_2019.RDS"))
 source(here("0_Parameters.R"))
 
 # Diseases considered
-dis_vec = c("mort", "cvd", "diab2", "dem")
+dis_vec = c("mort", "cvd", "bc", "cancer", "diab2", "dem", "dep")
+morbi_vec = c("cvd", "bc", "cancer", "diab2", "dem", "dep")
+
+################################################################################################################################
+#                                                4. DISEASES DATA PREPARATION                                                  #
+################################################################################################################################
+# For simplification of coding, rename dep_prevalence in dep_incidence but these are prevalences
+diseases_5 <-  diseases %>% 
+  rename (dep_incidence = dep_prevalence)
+
+# Age categories : 10 years
+diseases_5 <- diseases_5 %>%
+  mutate(
+    age_grp10 = case_when(
+      age_grp.x %in% c("20-24") ~ "20-24",
+      age_grp.x %in% c("25-29","30-34") ~ "25-34",
+      age_grp.x %in% c("35-39","40-44") ~ "35-44",
+      age_grp.x %in% c("45-49","50-54") ~ "45-54",
+      age_grp.x %in% c("55-59","60-64") ~ "55-64",
+      age_grp.x %in% c("65-69","70-74") ~ "65-75",
+      age_grp.x %in% c("75-79","80-84","85-89") ~ "75-89",
+      TRUE ~ NA_character_
+    )
+  )
+
+
+diseases_10 <- diseases_5 %>%
+  group_by(sex, age_grp10) %>%
+  summarise(
+    pop_age_sex = sum(pop_age_grp, na.rm = TRUE),
+    across(
+      .cols = paste0(morbi_vec, "_incidence"),
+      .fns  = ~ sum(.x, na.rm = TRUE),
+      .names = "{.col}"
+    ),
+    .groups = "drop"
+  )
+
 
 
 ################################################################################################################################
-#                                  4. CREATION OF SUBSET OF EMP SUBSET WITH ONLY VARIABLES NEEDED                              #
+#                                 5. CREATION OF SUBSET OF EMP DATASET WITH ONLY VARIABLES NEEDED                              #
 ################################################################################################################################
 
 # Creation of subset uniting variables of EMP and calculations
@@ -81,11 +118,12 @@ emp_subset <- emp %>%
 
 # Re-write nbkm_walking, easier to be used in functions
 emp_subset <- emp_subset %>%
-  rename(nbkm_walking = nbkm_walking_lower) %>% 
+  rename (nbkm_walking = nbkm_walking_lower) %>% 
 
 # Re-write sexe as female and male and convert as factors
-  mutate(sexe = as.character(sexe)) %>%                                 # Conversion in character for function to work well
-  mutate(sexe = fct_recode(sexe, "Male" = "1", "Female" = "2"))         # Replacing
+  mutate (sexe = as.character(sexe)) %>%                                 # Conversion in character for function to work well
+  mutate (sexe = fct_recode(sexe, "Male" = "1", "Female" = "2")) %>%     # Replacing
+  rename (sex = sexe)
 
 
 # Daily steps
@@ -101,40 +139,46 @@ emp_subset <- emp_subset %>%
 # Create age categories
 emp_subset <- emp_subset %>% 
   mutate(
-    age_grp.x = age_categories(
-      age,
-      lower = 0,
-      upper = 110,
-      by = 5)
-    ) %>% 
-  mutate(age_grp.x = as.character(age_grp.x))
+    age_grp10 = case_when(
+      age >= 20 & age <= 24 ~ "20-24",
+      age >= 25 & age <= 34 ~ "25-34",
+      age >= 35 & age <= 44 ~ "35-44",
+      age >= 45 & age <= 54 ~ "45-54",
+      age >= 55 & age <= 64 ~ "55-64",
+      age >= 65 & age <= 75 ~ "65-75",
+      age >= 75 & age <= 89 ~ "75-89",
+      TRUE ~ NA_character_
+    )
+  )
 
 
-# Add population counts per sex
-diseases <- diseases %>% 
-  rename(sexe = sex) %>% 
-  mutate(sexe = fct_recode(sexe, "Male" = "male", "Female" = "female"))         # Replacing
 
+# Add population counts per sex and age group
 emp_subset <- emp_subset %>% 
   left_join(
-      diseases %>% select(pop_age_grp, sexe, age_grp.x),    # Matching columns
-      by = c("sexe", "age_grp.x")                           # Fill the variables depending on sex and age group
-    ) %>% 
-  rename(pop_age_sex = pop_age_grp)
+      diseases_10 %>% 
+        select(pop_age_sex, sex, age_grp10),     # Matching columns
+        by = c("sex", "age_grp10")               # Fill the variables depending on sex and age group
+    )
 
 
-# Add diseases incidences
+# Add diseases incidences / prevalences
 emp_subset <- emp_subset %>% 
   left_join(
-    diseases %>% select(cc_incidence, dem_incidence, bc_incidence, cvd_incidence, diab2_incidence, sexe, age_grp.x),    # Matching columns
-    by = c("sexe", "age_grp.x")                                                                                         # Fill the variables depending on sex and age group
+    diseases_10 %>% 
+      select(cvd_incidence, bc_incidence, cancer_incidence, diab2_incidence, dem_incidence, dep_incidence, sex, age_grp10),    # Matching columns
+      by = c("sex", "age_grp10")                                                                                               # Fill the variables depending on sex and age group
   ) 
 
+
 # Add mortality rates
+insee <- insee %>% 
+  rename(sex = sexe)
+
 emp_subset <- emp_subset %>% 
   left_join(
-    insee %>% select(MR, sexe, age),       # Matching columns
-    by = c("sexe", "age")                  # Fill the variables depending on sexe and age
+    insee %>% select(MR, sex, age),       # Matching columns
+    by = c("sex", "age")                  # Fill the variables depending on sexe and age
   ) %>% 
   rename(mort_rate = MR)
 
@@ -144,8 +188,9 @@ emp_subset <-  emp_subset %>%
   mutate(mort_incidence = mort_rate * pop_age_sex)
 
 
-# Calculate incidence rates
-for (dis in dis_vec){
+
+# Calculate disease incidence rates
+for (dis in morbi_vec){
   emp_subset <- emp_subset %>%
     mutate(
       !!paste0(dis, "_rate") := if_else(
@@ -157,23 +202,24 @@ for (dis in dis_vec){
 }
 
 
+
 # Add life-expectancy for each sex
-for (i in 1:nrow(emp_subset)) {
-  emp_subset[i, "life_exp"] <- ifelse (
-    emp_subset$sexe[i]== "Female", 
-    85.99324,                            # Life expectancy for women = 85.99324
-    79.59503                             # Life expectancy for men = 79.59503
-    )  
-  }
+emp_subset <- emp_subset %>%
+  mutate(
+    life_exp = if_else(
+      sex == "Female",
+      85.99324,
+      79.59503
+    )
+  )
+
 
 # Add the years of life remaining, potentially affected by diseases or premature death
-for (i in 1:nrow(emp_subset)) {
-  emp_subset[i,"years_remaining"] <- ifelse(
-    emp_subset[i, "life_exp"]-emp_subset[i, "age"] >=0,
-    emp_subset[i, "life_exp"]-emp_subset[i, "age"],
-    0                                                   # No negatives for individuals above life expectancy
-    )
-}
+emp_subset <- emp_subset %>%
+  mutate(
+    years_remaining = pmax(life_exp - age, 0)
+  )
+
 
 # Only keep ages 20-89 years 
 emp_subset <-  emp_subset %>% 
