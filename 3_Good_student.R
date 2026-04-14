c7c773c7945b7a96a4dd6e4833a2cac174a9ef3c
inesoriany <ines.khoun@ens.psl.eu>
2026-04-14 11:44:36 +0200

    Clean repo without large files

---
+++ 3_Good_student.R
@@ -0,0 +1,20 @@
+#################################################
+#############     GOOD STUDENTS     #############
+#################################################
+
+
+
+################################################################################################################################
+#                                                    1. LOAD PACKAGES                                                          #
+################################################################################################################################
+pacman :: p_load(
+  rio,          # Data importation
+  here,         # Localization of files 
+  dplyr,        # Data management
+  srvyr,        # Survey
+  tidyr,        # Table - Data organization, extraction
+  tidyverse,    # Data manipulation and visualization
+  ggplot2       # Plotting
+)
+
+
---
+++ 4_Sensitivity_analysis.R
@@ -0,0 +1,7 @@
+#################################################
+########     SENSITIVITY ANALYSIS         #######
+#################################################
+
+# Modified parameters - files outputted :
+  # Walking speed 5.3 km/h
+  # Former DRF
