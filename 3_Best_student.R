5571ec5d28c0d082ea0068b78c83a8c325f1dbb3
inesoriany <ines.khoun@ens.psl.eu>
2026-04-28 17:13:57 +0200

    pyramid by area type

--- 3_Best_student.R
+++ 3_Best_student.R
@@ -57,7 +57,7 @@ bound_vec <- c("mid", "low", "up")
 
 
 ################################################################################################################################
-#                                                   4.                                                         #
+#                                                4. MAIN WALKING TRIPS DATASET                                                 #
 ################################################################################################################################
 # Only exclusively walking trips
   # Individual
@@ -69,7 +69,6 @@ bound_vec <- c("mid", "low", "up")
     filter(intermodal_walk_time == 0)
 
 # Sum steps by individual, area type, and disease
-# (avoiding double counting of trips using ident_dep)
 steps_by_individual_area_disease <- emp_main_walk_trip %>% 
   group_by(ident_ind, area_type, disease) %>% 
   summarise(
@@ -88,35 +87,6 @@ emp_main_walk_trip <- emp_main_walk_trip %>%
   )
 
 
-# Check if individuals meet the targets
-below_targets <- steps_by_individual_area_disease %>% 
-  mutate(
-    target = case_when(
-      area_type == "urban" ~ urban_target,
-      area_type == "periurban" ~ periurban_target,
-      area_type == "rural" ~ rural_target,
-      TRUE ~ NA_real_
-    ),
-    meets_target = total_steps >= target
-  ) %>% 
-  filter(!meets_target)
-
-cat("\n=== PERSONNES EN DESSOUS DES CIBLES ===\n")
-cat("Total (pondéré) :", sum(below_targets$pond_indc, na.rm = TRUE), "personnes\n")
-cat("Total (non pondéré) :", nrow(below_targets), "personnes\n\n")
-
-by_area <- below_targets %>% 
-  group_by(area_type) %>% 
-  summarise(
-    n_below_weighted = sum(pond_indc, na.rm = TRUE),
-    n_below_unweighted = n(),
-    mean_steps = mean(total_steps, na.rm = TRUE),
-    .groups = "drop"
-  )
-print(by_area)
-
-
-
 ################################################################################################################################
 #                                                   5. AGE DISTRIBUTION                                                        #
 ################################################################################################################################
@@ -130,12 +100,60 @@ main_jour <- emp_main_walkers %>%
 
 
 
-# Walking pyramid : Age distribution of walking volume for France (in steps)
+# Walking pyramid : Age distribution of walking volume for each territory (in steps)
 distrib_main_walk_EMP2019 <- main_jour %>% 
-  group_by(age_grp10) %>% 
+  group_by(age_grp10, area_type) %>% 
   summarise(mean_ind = survey_mean(step_commute, na.rm = TRUE))
 
 
-# Distribution coefficient
+# Distribution coefficient by area type
 distrib_main_walk_EMP2019 <- distrib_main_walk_EMP2019 %>% 
-  mutate(rho = mean_ind / mean_ind[age_grp10 == "20-29"])
+  group_by(area_type) %>% 
+  mutate(rho = mean_ind / mean_ind[age_grp10 == "20-29"]) %>% 
+  ungroup()
+
+emp_main_walk_trip <- emp_main_walk_trip %>% 
+  left_join(distrib_main_walk_EMP2019 %>% 
+  select(age_grp10, rho), by = "age_grp10")
+
+
+# Calculate target to reach for each individual depending on their area type and age group
+emp_main_walk_trip <- emp_main_walk_trip %>% 
+  mutate(target_step = rho * case_when(
+    area_type == "urban" ~ urban_target,
+    area_type == "periurban" ~ periurban_target,
+    area_type == "rural" ~ rural_target,
+    TRUE ~ NA_real_
+  ) * )
+
+
+# Filter individuals below targets ? and do HIA (to know the added value)
+  ################## See how to integrate that later ----
+  # Check if individuals meet the targets
+  below_targets <- emp_main_walk_trip %>% 
+    mutate(
+      target = case_when(
+        area_type == "urban" ~ urban_target,
+        area_type == "periurban" ~ periurban_target,
+        area_type == "rural" ~ rural_target,
+        TRUE ~ NA_real_
+      ),
+      meets_target = total_steps >= target
+    ) %>% 
+    filter(!meets_target)
+
+  cat("\n=== PERSONNES EN DESSOUS DES CIBLES ===\n")
+  cat("Total (pondéré) :", sum(below_targets$pond_indc, na.rm = TRUE), "personnes\n")
+  cat("Total (non pondéré) :", nrow(below_targets), "personnes\n\n")
+
+  by_area <- below_targets %>% 
+    group_by(area_type) %>% 
+    summarise(
+      n_below_weighted = sum(pond_indc, na.rm = TRUE),
+      n_below_unweighted = n(),
+      mean_steps = mean(total_steps, na.rm = TRUE),
+      .groups = "drop"
+    )
+  print(by_area)
+
+# data preparation for HIA
